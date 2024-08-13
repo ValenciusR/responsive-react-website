92fa8a64e3e786db45cfd5dc1bc380c018e72869
ValenciusR <valencius.rianto25@gmail.com>
2024-08-13 09:46:32 +0700

    1.20.50

--- src/components/NavbarComponent.js
+++ src/components/NavbarComponent.js
@@ -1,16 +1,36 @@
 import React from "react";
+import { useState, useEffect } from "react";
+
 import { Navbar, Container, Nav } from "react-bootstrap";
 import { navLinks } from "../data/index";
 import { NavLink } from "react-router-dom";
 
 function NavbarComponent() {
+  const [changeColor, setChangeColor] = useState(false);
+
+  const changeBackgroundColor = () => {
+    if (window.scrollY > 10) {
+      setChangeColor(true);
+    } else {
+      setChangeColor(false);
+    }
+  };
+
+  useEffect(() => {
+    changeBackgroundColor();
+
+    window.addEventListener("scroll", changeBackgroundColor);
+  });
+
   return (
-    <Navbar expand="lg">
+    <Navbar expand="lg" className={changeColor ? "color-active" : ""}>
       <Container>
-        <Navbar.Brand href="#home">Ngoding.In</Navbar.Brand>
+        <Navbar.Brand href="#home" className="fs-3 fw-bold">
+          Ngoding.In
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mx-auto">
+          <Nav className="mx-auto text-center">
             {navLinks.map((link) => {
               return (
                 <div className="nav-link" key={link.id}>
@@ -19,6 +39,7 @@ function NavbarComponent() {
                     className={({ isActive, isPending }) =>
                       isPending ? "pending" : isActive ? "active" : ""
                     }
+                    end
                   >
                     {link.text}
                   </NavLink>
@@ -27,8 +48,10 @@ function NavbarComponent() {
             })}
           </Nav>
 
-          <div>
-            <button>Join With Us</button>
+          <div className="text-center">
+            <button className="btn btn-outline-danger rounded-1">
+              Join With Us
+            </button>
           </div>
         </Navbar.Collapse>
       </Container>
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,9 +1,35 @@
 import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import HeroImage from "../assets/img/hero.png";
 
 function Homepage() {
   return (
     <div className="homepage">
-      <header className="w-100 min-vh-100"></header>
+      <header className="w-100 min-vh-100 d-flex align-items-center">
+        <Container>
+          <Row className="header-box d-flex align-items-center">
+            <Col>
+              <h1 className="mb-4">
+                Temukan <br /> <span>Course Favoritmu</span> Bersama Kami!
+              </h1>
+              <p className="mb-4">
+                Ullamco reprehenderit aliqua enim nulla do tempor do adipisicing
+                pariatur cillum ea in est.
+              </p>
+              <button className="btn btn-danger btn-lg rounded-1 me-2">
+                Lihat Kelas
+              </button>
+              <button className="btn btn-outline-danger btn-lg rounded-1">
+                Lihat Promo
+              </button>
+            </Col>
+            <Col>
+              <img src={HeroImage} alt="hero-img" />
+            </Col>
+          </Row>
+        </Container>
+      </header>
+      <div className="kelas w-100 min-vh-100"></div>
     </div>
   );
 }
