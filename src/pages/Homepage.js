16c0eb3f22e118ec38ee6fb96b78894b7cf6889c
ValenciusR <valencius.rianto25@gmail.com>
2024-08-14 09:46:14 +0700

    1.29.18

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -8,7 +8,7 @@ function Homepage() {
       <header className="w-100 min-vh-100 d-flex align-items-center">
         <Container>
           <Row className="header-box d-flex align-items-center">
-            <Col>
+            <Col lg="6">
               <h1 className="mb-4">
                 Temukan <br /> <span>Course Favoritmu</span> Bersama Kami!
               </h1>
@@ -16,14 +16,14 @@ function Homepage() {
                 Ullamco reprehenderit aliqua enim nulla do tempor do adipisicing
                 pariatur cillum ea in est.
               </p>
-              <button className="btn btn-danger btn-lg rounded-1 me-2">
+              <button className="btn btn-danger btn-lg rounded-1 me-2 mb-xs-0 mb-2">
                 Lihat Kelas
               </button>
-              <button className="btn btn-outline-danger btn-lg rounded-1">
+              <button className="btn btn-outline-danger btn-lg rounded-1 mb-xs-0 mb-2">
                 Lihat Promo
               </button>
             </Col>
-            <Col>
+            <Col lg="6" className="pt-lg-0 pt-5">
               <img src={HeroImage} alt="hero-img" />
             </Col>
           </Row>
