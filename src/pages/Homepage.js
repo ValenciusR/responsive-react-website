018870c590babf2b99a636b29508103bd61188a7
ValenciusR <valencius.rianto25@gmail.com>
2024-08-15 16:46:23 +0700

    2.19.06

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -2,10 +2,24 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import HeroImage from "../assets/img/hero.png";
 
+import { kelasTerbaru, dataSwiper } from "../data/index";
+import { useNavigate } from "react-router-dom";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+// import required modules
+import { Pagination } from "swiper/modules";
+
 function Homepage() {
+  let navigate = useNavigate();
+
   return (
     <div className="homepage">
-      <header className="w-100 min-vh-100 d-flex align-items-center">
+      <header className="w-100 min-vh-100 d-flex align-items-center pt-lg-5">
         <Container>
           <Row className="header-box d-flex align-items-center">
             <Col lg="6">
@@ -29,7 +43,110 @@ function Homepage() {
           </Row>
         </Container>
       </header>
-      <div className="kelas w-100 min-vh-100"></div>
+      <div className="kelas w-100 min-vh-100">
+        <Container>
+          <Row>
+            <Col>
+              <h1 className="text-center fw-bold">Kelas Terbaru</h1>
+              <p className="text-center">
+                Officia excepteur ut incididunt laborum aliqua officia ut
+                laboris incididunt consectetur adipisicing.
+              </p>
+            </Col>
+          </Row>
+          <Row>
+            {kelasTerbaru.map((kelas) => {
+              return (
+                <Col key={kelas.id} className="shadow rounded">
+                  <img
+                    src={kelas.image}
+                    alt="unsplash.com"
+                    className="mb-5 rounded-top"
+                  />
+                  <div className="star mb-2 px-3">
+                    <i className={kelas.star1}></i>
+                    <i className={kelas.star2}></i>
+                    <i className={kelas.star3}></i>
+                    <i className={kelas.star4}></i>
+                    <i className={kelas.star5}></i>
+                  </div>
+                  <h5 className="mb-5 px-3">{kelas.title}</h5>
+                  <div className="ket d-flex align-items-center justify-content-between px-3 pb-3">
+                    <p className="m-0 text-primary">{kelas.price}</p>
+                    <button className="btn btn-danger rounded-1">
+                      {kelas.buy}
+                    </button>
+                  </div>
+                </Col>
+              );
+            })}
+          </Row>
+          <Row>
+            <Col className="text-center">
+              <button
+                className="btn btn-success rounded-5 btn-lg"
+                onClick={() => navigate("/kelas")}
+              >
+                Lihat Semua Kelas{" "}
+                <i className="fa-solid fa-chevron-right ms-1"></i>
+              </button>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+      <div className="testimonial py-5">
+        <Container>
+          <Row>
+            <Col>
+              <h1 className="text-center fw-bold my-5">Testimonial</h1>
+            </Col>
+          </Row>
+          <Row>
+            <Swiper
+              slidesPerView={1}
+              spaceBetween={10}
+              pagination={{
+                clickable: true,
+              }}
+              breakpoints={{
+                640: {
+                  slidesPerView: 1,
+                  spaceBetween: 20,
+                },
+                768: {
+                  slidesPerView: 2,
+                  spaceBetween: 40,
+                },
+                992: {
+                  slidesPerView: 2,
+                  spaceBetween: 50,
+                },
+                1200: {
+                  slidesPerView: 3,
+                  spaceBetween: 50,
+                },
+              }}
+              modules={[Pagination]}
+              className="mySwiper"
+            >
+              {dataSwiper.map((data) => {
+                return (
+                  <SwiperSlide key={data.id} className="shadow-sm">
+                    <p className="desc">{data.desc}</p>
+                    <div className="people">
+                      <img src={data.image} alt=""></img>
+                      <div>
+                        <h5 className="mb-1">{data.name}</h5>
+                        <p className="m-0 fw-bold">{data.skill}</p>
+                      </div>
+                    </div>
+                  </SwiperSlide>
+                );
+              })}
+            </Swiper>
+          </Row>
+        </Container>
+      </div>
     </div>
   );
 }
